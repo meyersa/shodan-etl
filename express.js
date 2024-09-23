e053d4ff8a1c3332be0b65cdcc8a368888f72217
Meyers <augustmeyers14@gmail.com>
2024-09-23 16:20:10 -0400

    Implement caching to save mongo queries

--- express.js
+++ express.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const path = require("path");
+const NodeCache = require("node-cache");
 const { MongoClient } = require("mongodb");
+const { stat } = require("fs");
 
 const app = express();
+const cache = new NodeCache();
 
 // Get credentials from ENV
 const { MONGO_URL, MONGO_DB } = process.env;
@@ -35,6 +38,9 @@ let ipCollection;
     console.error("Unable to connect to MongoDB, ignore if this was during initial build", e);
     process.exit(1);
   }
+
+  console.log(`Startup finished \n\n\n`)
+
 })();
 
 // Serve the specific CSS file using a custom route
@@ -42,124 +48,128 @@ app.get("/express.css", (req, res) => {
   res.sendFile(path.join(__dirname, "express.css"));
 });
 
+async function getInfo() {
+  console.log("--- Getting Shodan information ---");
+
+  if (!ipCollection) {
+    throw new Error("MongoDB connection not established");
+  }
+
+  console.log("Getting total document count");
+  const totalCountCursor = ipCollection.countDocuments({});
+  const totalcount = await totalCountCursor;
+
+  console.log("Getting open ports and counts");
+  const openPortsCursor = ipCollection.aggregate([
+    {
+      $project: {
+        data: {
+          $objectToArray: "$data",
+        },
+      },
+    },
+    {
+      $unwind: "$data",
+    },
+    {
+      $group: {
+        _id: "$data.v.port",
+        count: { $sum: 1 },
+      },
+    },
+    {
+      $sort: { count: -1 },
+    },
+    {
+      $limit: 5,
+    },
+  ]);
+  const openPorts = await openPortsCursor.toArray();
+
+  console.log("Getting unique countries and their count");
+  const uniqueCountriesCursor = ipCollection.aggregate([
+    { $group: { _id: "$location.country", count: { $sum: 1 } } },
+    { $sort: { count: -1 } },
+  ]);
+  const uniqueCountries = await uniqueCountriesCursor.toArray();
+
+  console.log("Getting most popular content and its count");
+  const mostPopularContentCursor = ipCollection.aggregate([
+    {
+      $project: {
+        data: {
+          $objectToArray: "$data",
+        },
+      },
+    },
+    {
+      $unwind: "$data",
+    },
+    {
+      $group: {
+        _id: "$data.v.data",
+        count: { $sum: 1 },
+      },
+    },
+    {
+      $sort: { count: -1 },
+    },
+    {
+      $limit: 3,
+    },
+  ]);
+  const mostPopularContent = await mostPopularContentCursor.toArray();
+
+  console.log("Getting last 3 records")
+  const lastThreeRecordsCursor = ipCollection.find().sort({ _id: -1 }).limit(3);
+  const lastThreeRecords = await lastThreeRecordsCursor.toArray();
+
+  // Filter out null and empty string entries from mostPopularContent
+  const validMostPopularContent = mostPopularContent.filter((content) => content._id !== null && content._id !== "");
+
+  // Replace null and empty string with "Null"
+  const formattedMostPopularContent = validMostPopularContent.map((content) => ({
+    _id: content._id || "Null",
+    count: content.count,
+  }));
+
+  console.log("Getting the number of bans")
+  const bansCursor = ipCollection.countDocuments({
+    $or: [
+      { banned: true }, // Check if the document has the "banned" field set to true
+    ],
+  });
+  const totalBans = await bansCursor;
+
+  console.log("--- Returning Mongo Statistics ---")
+  // Prepare statistics
+  return (statistics = {
+    total_count: totalcount,
+    open_ports: openPorts,
+    unique_countries: uniqueCountries,
+    most_popular_content: formattedMostPopularContent,
+    last_three_records: lastThreeRecords,
+    ban_count: totalBans,
+  });
+}
+
 app.get("/", async (req, res) => {
   try {
-    if (!ipCollection) {
-      throw new Error("MongoDB connection not established");
+    console.log("Returning /")
+
+    var statistics = undefined;
+
+    statistics = cache.get("statistics");
+
+    if (!statistics) {
+      console.log("Cache miss")
+      statistics = await getInfo();
+      cache.set("statistics", statistics, 300);
+
+    } else {
+      console.log("Cache hit")
+
     }
-
-    console.log("Getting total document count");
-    const totalCountCursor = ipCollection.countDocuments({});
-    const totalcount = await totalCountCursor;
-
-    console.log("Getting open ports and counts");
-    const openPortsCursor = ipCollection.aggregate([
-      {
-        $project: {
-          data: {
-            $objectToArray: "$data",
-          },
-        },
-      },
-      {
-        $unwind: "$data",
-      },
-      {
-        $group: {
-          _id: "$data.v.port",
-          count: { $sum: 1 },
-        },
-      },
-      {
-        $sort: { count: -1 },
-      },
-      {
-        $limit: 5,
-      },
-    ]);
-    const openPorts = await openPortsCursor.toArray();
-
-    console.log("Getting unique countries and their count");
-    const uniqueCountriesCursor = ipCollection.aggregate([
-      { $group: { _id: "$location.country", count: { $sum: 1 } } },
-      { $sort: { count: -1 } },
-    ]);
-    const uniqueCountries = await uniqueCountriesCursor.toArray();
-
-    console.log("Get most popular content and its count");
-    const mostPopularContentCursor = ipCollection.aggregate([
-      {
-        $project: {
-          data: {
-            $objectToArray: "$data",
-          },
-        },
-      },
-      {
-        $unwind: "$data",
-      },
-      {
-        $group: {
-          _id: "$data.v.data",
-          count: { $sum: 1 },
-        },
-      },
-      {
-        $sort: { count: -1 },
-      },
-      {
-        $limit: 3,
-      },
-    ]);
-    const mostPopularContent = await mostPopularContentCursor.toArray();
-
-    // Get the last 3 records
-    const lastThreeRecordsCursor = ipCollection.find().sort({ _id: -1 }).limit(3);
-    const lastThreeRecords = await lastThreeRecordsCursor.toArray();
-
-    // Filter out null and empty string entries from mostPopularContent
-    const validMostPopularContent = mostPopularContent.filter((content) => content._id !== null && content._id !== "");
-
-    // Replace null and empty string with "Null"
-    const formattedMostPopularContent = validMostPopularContent.map((content) => ({
-      _id: content._id || "Null",
-      count: content.count,
-    }));
-
-    // Prepare statistics
-    const statistics = {
-      total_count: totalcount,
-      open_ports: openPorts,
-      unique_countries: uniqueCountries,
-      most_popular_content: formattedMostPopularContent,
-      last_three_records: lastThreeRecords,
-    };
-
-    // Get the number of bans and no bans
-    const bansCursor = ipCollection.countDocuments({
-      $or: [
-        { banned: true }, // Check if the document has the "banned" field set to true
-        {
-          $or: statistics.last_three_records.map((record) => ({
-            [`ban_${record.ban_id}`]: { $exists: true },
-          })),
-        }, // Check for each ban_id property
-      ],
-    });
-    const noBansCursor = ipCollection.countDocuments({
-      $and: [
-        { banned: { $ne: true } }, // Check if the document does not have the "banned" field set to true
-        {
-          $nor: [
-            // Ensure none of the ban properties exist
-            ...statistics.last_three_records.map((record) => ({
-              [`ban_${record.ban_id}`]: { $exists: true },
-            })),
-          ],
-        },
-      ],
-    });
-    const [bansCount, noBansCount] = await Promise.all([bansCursor, noBansCursor]);
 
     // Render the statistics as HTML on the home page
     res.send(`
@@ -234,17 +244,25 @@ app.get("/", async (req, res) => {
           <h2>Crowdsec Bans:</h2>
           <div class="bars">
             ${
-              bansCount !== undefined
+              statistics.ban_count !== undefined
                 ? `
                 <div class="bar">
                 <span>No Bans</span>
-                <div class="bar-fill" style="width: ${(noBansCount / (bansCount + noBansCount || 1)) * 100}%;"></div>
-                <span>${noBansCount}</span>
+                <div class="bar-fill" style="width: ${
+                  ((statistics.total_count - statistics.ban_count) /
+                    (statistics.ban_count + (statistics.total_count - statistics.ban_count) || 1)) *
+                  100
+                }%;"></div>
+                <span>${statistics.total_count - statistics.ban_count}</span>
               </div>
               <div class="bar">
                 <span>Bans</span>
-                <div class="bar-fill" style="width: ${(bansCount / (bansCount + noBansCount || 1)) * 100}%;"></div>
-                <span>${bansCount}</span>
+                <div class="bar-fill" style="width: ${
+                  (statistics.ban_count /
+                    (statistics.ban_count + (statistics.total_count - statistics.ban_count) || 1)) *
+                  100
+                }%;"></div>
+                <span>${statistics.ban_count}</span>
               </div>
                   `
                 : "N/A"
